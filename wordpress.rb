356c2d9bc47da553a76eccef7dad3119b6d62de5
haruyaishikawa <toraudonn@gmail.com>
2017-03-28 17:34:08 +0900

    update so far

--- wordpress.rb
+++ wordpress.rb
@@ -1,6 +1,8 @@
 require 'csv'
 
-example = Dir["./hoge_name/*.csv"][0]
+dir_name = "./hoge_name/"
+
+example = Dir[dir_name+"*.csv"][0]
 month = example.split('/')[2].split('_')[1].to_i.to_s
 day = example.split('/')[2].split('_')[2].to_i.to_s
 date_name = month + '月' + day + '日'
@@ -9,6 +11,7 @@ text = "<h2>"+date_name+"に公開された論文"+"</h2>"
 
 Dir["./hoge_name/*.csv"].each do |f|
   cat = f.split('/')[2].split('_')[3].split('.')[0]
+  p cat
   text += "<h3>"+cat+"</h3><ul>"
   data = CSV.read(f, headers: true)
   data.each do |d|
@@ -19,17 +22,21 @@ Dir["./hoge_name/*.csv"].each do |f|
     title = d[5]
     summary = d[7]
     authors = d[9]
-    p authors
-    author_list = ""
-    authors.each do |a|
-      author_list = author_list + a + ", "
-    end
+    authors = authors.gsub("[","").gsub("]","").gsub("'","")
     
-    text = text + "<li><a href='" + abs_link + "'>" + title + "</a><ul><li>著者：" + author_list + "</li></ul>"
+    text = text + "<li><a href='" + abs_link + "'>" + title + "</a><ul><li>著者：" + authors + "</li></ul>"
   end
   text += "</ul>"
-  
-  p text
+end
+
+begin
+  file = File.open(dir_name+"daily.txt", "w")
+  file.write(text) 
+rescue IOError => e
+  #some error occur, dir not writable etc.
+  p "error when saving to file"
+ensure
+  file.close unless file.nil?
 end
 
 
